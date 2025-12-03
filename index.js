1c520f2f62001bff408d6f8ed93ef556f2b19f1f
Maisie90 <maisiebishop@yahoo.co.uk>
2025-12-03 10:02:03 +0000

    added .env to project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config() // calling .env file
 const app = require("./app")
-const port = 3000
+const port = process.env.PORT  // PORT is in the .env file
 
 app.listen(port, () => {                            
   console.log(`FruityApp listening on port ${port}`)  
